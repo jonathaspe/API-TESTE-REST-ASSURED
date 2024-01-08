b1d575c2427de7e46771560747d1b2c9e537eb06
leafar-Santos <leafarccomputacao@gmail.com>
2024-01-08 14:50:46 -0300

    Ajuste da massa de dados

--- src/main/java/com/santos/ApiTesetRestAssuredApplication.java
+++ src/main/java/com/santos/ApiTesetRestAssuredApplication.java
@@ -49,7 +49,7 @@ public class ApiTesetRestAssuredApplication implements CommandLineRunner {
 		
 		
 		
-		usuarioRepository.saveAll(Arrays.asList(u1,u2,u3,u4,u5));
+		usuarioRepository.saveAll(Arrays.asList(u1,u2,u3,u4,u5,u6,u7,u8,u9,u10,u11,u12,u13,u14,u15,u16,u17,u18));
 		
 	}
 
